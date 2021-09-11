c1ef24404ad0f978f06dbd0c97539662d2395826
Saurabh <saurabhsingh4778698@gmail.com>
2021-09-11 19:25:49 +0530

    navbar dark mode fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar(props) {
     
     return (
         <>
-            <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`} style={{boxShadow:' rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}>
+            {/* <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`} style={{boxShadow:' rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}>
                 <div className="container-fluid">
                     <a className="navbar-brand text-danger" href="/">{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,11 +27,44 @@ export default function Navbar(props) {
                     </div>
 
                    
-                    
+                    <form className="d-flex">
+                     <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                     <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
                     </div>
                 </div>
-          </nav>
+          </nav> */}
+
+
+            <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`} style={{boxShadow:' rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px'}}>
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">{props.title}</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    
+                </ul>
+                <div className={`form-check form-switch me-5 text-${props.mode==='light'?'dark':'light'}`}>
+                    <input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked" onClick={props.toggleMode}/>
+                    <label className="form-check-label" htmlFor="flexSwitchCheckChecked" >Dark Mode</label>
+                </div>
+                
+                
+                </div>
+            </div>
+            </nav>
         </>
+
     )
 }
+
+
+
+
+
 // export default Navbar;
